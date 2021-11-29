41fa773bad99a60f81fb6607d1a3b0c32c2bf324
marodseg <marodseg@correo.ugr.es>
2021-11-29 19:15:50 +0100

    Update files

--- Práctica 3/src/main.c
+++ Práctica 3/src/main.c
@@ -2,38 +2,31 @@
 #include <stdlib.h>
 
 extern FILE *yyin;
-
 int yyparse(void);
 
-FILE *abrir_entrada(int argc, char *argv[]){
-
+FILE *abrir_entrada(int argc, char *argv[])
+{
 	FILE *f = NULL;
 
-	if (argc > 1){
-
+	if (argc > 1)
+	{
 		f = fopen(argv[1], "r");
-
-		if (f==NULL){
-
+		if (f==NULL)
+		{
 			fprintf(stderr, "No se ha reconocido el fichero de entrada\n");
 			exit(1);
-
-		}else{
-			printf("\nLeyendo fichero...\n\n");
 		}
-
-	}else{
-		printf("\nLeyendo entrada estandar.\n\n");
+		else
+			printf("\nLeyendo fichero...\n\n");
 	}
-
+	else
+		printf("\nLeyendo entrada estandar.\n\n");
 	return f;
-
 }
 
+/*******************************************************************************/
+
 int main( int argc, char *argv[1]){
-
 	yyin = abrir_entrada(argc, argv);
-
 	return yyparse();
-
 }
